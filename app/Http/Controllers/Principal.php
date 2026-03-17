1ab3cb4d68253214708768c6f6ac167b9b59ad61
Hugglermatheus125 <matheushuggler125@gmail.com>
2026-03-17 14:48:30 -0300

    manipulacao_rotas

--- app/Http/Controllers/Principal.php
+++ app/Http/Controllers/Principal.php
@@ -10,5 +10,24 @@ class Principal extends Controller
         echo 'Página Principal';
     }
 
-    
+    function contato(string $nome){
+        echo $nome;
+    }
+
+    function contatoNomeCompleto(string $nome, string $sobrenome){
+        echo "O contato é $nome $sobrenome";
+    }
+
+    function contatoMensagem(string $nome, string $sobrenome, string $mensagem){
+        echo "O contato $nome $sobrenome enviou uma mensagem:";
+        echo "<h1>$mensagem</h1>";
+    }
+
+    function contatoTelefone(string $nome, string $sobrenome, string $mensagem, string $telefone, string $email = 'Email não informado'){
+        echo "O contato $nome $sobrenome enviou uma mensagem:";
+        echo "<h1>$mensagem</h1>";
+        echo "<h1>Telefone: $telefone</h1>";
+        echo "<h1>Email: $email</h1>";
+    }
+
 }
