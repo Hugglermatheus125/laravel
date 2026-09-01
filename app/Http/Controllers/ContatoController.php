b23be63922f1af4156e157f1d4dbd86d4d3858b0
Hugglermatheus125 <matheushuggler125@gmail.com>
2026-09-01 17:46:44 -0300

    comitititit

---
+++ app/Http/Controllers/ContatoController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ContatoController extends Controller
+{
+    function principal() {
+        echo "Teste";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,5 @@ use App\Http\Middleware\LogAcessoMiddleware;
 */
 
 Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
+Route::get('/contato', [App\Http\Controllers\ContatoController::class, 'principal'])
+->middleware(LogAcessoMiddleware::class)->name("contato");
